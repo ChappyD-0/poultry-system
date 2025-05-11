c22597d8da175fbc74431b2e4e4d5214fc23bd0e
ChappyD-0 <olopez73@outlook.com>
2025-05-10 22:55:14 -0600

    Create branchs oscar_dev

--- src/main/java/com/blacksystem/poultry_system/PoultrySystemApplication.java
+++ src/main/java/com/blacksystem/poultry_system/PoultrySystemApplication.java
@@ -2,7 +2,7 @@ package com.blacksystem.poultry_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Oscar's Branch 
 @SpringBootApplication
 public class PoultrySystemApplication {
 
